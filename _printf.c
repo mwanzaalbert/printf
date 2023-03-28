b42acc068f3f01e15308413980810cff32be9908
reckafella <ethanmuthoniw6@gmail.com>
2023-03-28 23:28:35 +0300

    update _prinf.c && handlers.c

--- _printf.c
+++ _printf.c
@@ -29,6 +29,12 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 	int i, wid, prec, ret = 0;
 	char tmp;
 	unsigned char flags, len;
+
+	/**
+	 * f - pointer to a function
+	 *
+	 * Return: int;
+	 */
 	unsigned int (*f)(va_list, buffer_t *,
 			unsigned char, int, int, unsigned char);
 
--- handlers.c
+++ handlers.c
@@ -4,8 +4,6 @@ unsigned char handle_flags(const char *flag, char *index);
 unsigned char handle_length(const char *modifier, char *index);
 int handle_width(va_list args, const char *modifier, char *index);
 int handle_precision(va_list args, const char *modifier, char *index);
-unsigned int (*handle_specifiers(const char *specifier))(va_list, buffer_t *,
-		unsigned char, int, int, unsigned char);
 
 /**
  * handle_flags - Matches flags with corresponding values.
@@ -158,9 +156,10 @@ int handle_precision(va_list args, const char *modifier, char *index)
 }
 
 /**
- * handle_specifiers - Matches a conversion specifier with
- *                     a corresponding conversion function.
- * @specifier: A pointer to a potential conversion specifier.
+ * handle_specifiers - Matches a conversion specifier with a
+ * corresponding conversion function.
+ * @int: integer
+ * @char: character
  *
  * Return: If a conversion function is matched - a pointer to the function.
  *         Otherwise - NULL.
